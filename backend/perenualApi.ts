484f759e1fa083d92fb8b331ba57277cc2d85351
Volan <142726679+Volanlmao@users.noreply.github.com>
2025-09-04 19:19:27 +0200

    Handle missing Appwrite session

--- backend/perenualApi.ts
+++ backend/perenualApi.ts
@@ -1,10 +1,8 @@
 // src/backend/perenualApi.ts
 import axios from "axios";
+import { PERENUAL_API_TOKEN } from "@env";
 
-export const ApiToken = "sk-RX4y67f93acb4795d9734"; // main token
-// export const ApiToken = ""; 
-// export const ApiToken = "sk-2EvB67fd3996bb0819785";
-// export const ApiToken = "sk-njwe67fd37a9ded179784";
+export const ApiToken = PERENUAL_API_TOKEN;
 
 export const perenualApi = axios.create({
   baseURL: "https://perenual.com/api/v2",
